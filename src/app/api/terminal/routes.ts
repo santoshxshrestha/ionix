dd9677e7b47f8bd1b035050daf81f95625e52c0c
santoshxshrestha <santoshxshrestha@gmail.com>
2026-01-14 20:46:12 +0545

    what end

---
+++ src/app/api/terminal/routes.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { exec } from "child_process";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { command } = await request.json();
+    const output = await runComand(command);
+    return new NextResponse(output, { status: 200 });
+  } catch (error) {
+    console.log("Error", error);
+    return new NextResponse("Error processing the message", {
+      status: 400,
+    });
+  }
+}
+
+async function runComand(command: string) {
+  return new Promise<string>((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(`Error: ${error.message}`);
+      } else if (stderr) {
+        reject(`Stderr: ${stderr}`);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
