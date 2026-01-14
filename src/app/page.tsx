aec175758fcdb7c137df4f06269f587a7096eef2
santoshxshrestha <santoshxshrestha@gmail.com>
2026-01-14 20:42:59 +0545

    ui: base term

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-
+'use client';
+import { useState } from 'react';
 export default function terminal() {
+    const [command, setCommand] = useState('');
     return (
     <>
-            fuck you main
+            <main className="min-h-screen justify-between p-24 bg-black text-green-500">
+                <div className="flex items-center space-x-2">
+                <span className="">user@nextjs:~$ </span>
+                <input
+                    type="text"
+                    value={command}
+                    onChange={(e) => setCommand(e.target.value)}
+                    className="bg-black text-green-500 focus:outline-none w-full"
+                    autoFocus
+                />
+                </div>
+            </main>
     </>
     )
 }
